062569caa1ff776e575fb608b435116472391aa9
maricosi <marisaisimoes@gmail.com>
2015-06-22 23:18:33 +0100

    Rest and soap

---
+++ playlist-ejbLyrics/src/main/java/ar/p4/ejb/webserviceRest/GetLyricResult.java
@@ -0,0 +1,21 @@
+package ar.p4.ejb.webserviceRest;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement( name="GetLyricResult", namespace="http://api.chartlyrics.com/")
+public class GetLyricResult {
+
+	@XmlElement(name="Lyric")
+	private String lyricMusic;
+
+	public String getLyric() {
+		return lyricMusic;
+	}
+
+	public void setLyric(String lyric) {
+		this.lyricMusic = lyric;
+	}
+	
+	
+}
---
+++ playlist-ejbLyrics/src/main/java/ar/p4/ejb/webserviceRest/LyricsREST.java
@@ -0,0 +1,31 @@
+package ar.p4.ejb.webserviceRest;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+
+public class LyricsREST {
+
+	public static void main(String[] args) {
+
+		while(true){
+			try{
+				String song="Soul Kitchen";
+				String artist="The Doors";
+				ResteasyClient cliente= new ResteasyClientBuilder().build();
+				ResteasyWebTarget target=cliente.target("http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist="+artist+"&song="+song);
+				Response response= target.request(MediaType.APPLICATION_XML).get();
+				String lyrics= response.readEntity(GetLyricResult.class).getLyric();
+				System.out.println(lyrics);
+				return;
+			} catch(Exception e){
+
+			}
+		}
+	}
+
+
+}
