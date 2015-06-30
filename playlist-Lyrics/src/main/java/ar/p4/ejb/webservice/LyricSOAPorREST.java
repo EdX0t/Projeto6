d9805a4b85fa709a06a0d90546adf41213f6061e
maricosi <marisaisimoes@gmail.com>
2015-06-30 22:41:38 +0100

    REST SOAP 3 maneiras

--- playlist-Lyrics/src/main/java/ar/p4/ejb/webservice/LyricSOAPorREST.java
+++ playlist-Lyrics/src/main/java/ar/p4/ejb/webservice/LyricSOAPorREST.java
@@ -10,6 +10,8 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
+import ar.p4.ejb.webserviceRestWikia.LyricsResult;
+
 import com.chartlyrics.api.SOAP.Apiv1;
 import com.chartlyrics.api.SOAP.Apiv1Soap;
 import com.chartlyrics.api.SOAP.ArrayOfSearchLyricResult;
@@ -19,36 +21,37 @@ import com.chartlyrics.api.SOAP.SearchLyricResult;
 
 @Stateless
 public class LyricSOAPorREST {
-	
 
 	public  String lyricOfMusic(String artist, String song){
-		String lyric="";
+		String lyric;
 		lyric=lyricSOAP(artist,song);
+		if(lyric!=null)return lyric;
+
+		lyric=lyricRESTApi (artist,song);
+		if(lyric!=null) return lyric;
+
+		lyric=lyricRESTWiki (artist,song);
+		if(lyric!=null)	return lyric;
 		
-		if(lyric!=null){
-			return lyric;
-		}else {
-			return lyricREST (artist,song);
-		}	
+		return null;
 	}
 
+
 	private static String lyricSOAP(String artist, String song) {
-		
+
 		Apiv1 api = new Apiv1();
 		Apiv1Soap soap =api.getApiv1Soap();
 		ArrayOfSearchLyricResult arrayResult = null;
 		int contador = 0;
-		int maxnum=30;
+		int maxnum=15;
 
 		while(contador<maxnum){
 			try {
 				arrayResult = soap.searchLyric(artist, song);
-				System.out.println("Lyric1: \n"+arrayResult.getSearchLyricResult());
 				if(arrayResult.getSearchLyricResult().get(0) == null) {
-					System.out.println("Não encontra!!!");
 					return null;
 				}
-				
+				break;
 			} catch (Exception e) {
 				//				e.printStackTrace();
 				System.out.println("Connecting...");
@@ -58,16 +61,14 @@ public class LyricSOAPorREST {
 		if (contador==maxnum){
 			return null;
 		}
-		
+
 		int idLyric=-1;
 		String lyricCheckSum="";
 		try{
 			for(SearchLyricResult result:arrayResult.getSearchLyricResult() ){
-				System.out.println(result.getSong());
 				if(result.getSong().equalsIgnoreCase(song)){
 					idLyric=result.getLyricId();
 					lyricCheckSum=result.getLyricChecksum();
-					System.out.println("ID DA LIRICA"+idLyric);
 					break;
 				}
 			}
@@ -78,24 +79,23 @@ public class LyricSOAPorREST {
 		while(contador<maxnum){
 			try{
 				GetLyricResult result = soap.getLyric(idLyric, lyricCheckSum);
-				System.out.println("Lyric: \n"+result.getLyric());
 				return result.getLyric();
 			} catch (Exception e){
 				System.out.println("não fizeste nada!!!");
 			}
+			contador++;
 		}
 		return null;
-		
 	}
 
-	private static String lyricREST(String artist, String song)  {
+	private static String lyricRESTApi(String artist, String song)  {
 
 		int idLyric=-1;
 		String lyricCheckSum="";
 		ResteasyClient cliente= new ResteasyClientBuilder().build();
 		List <SearchLyricResult> listResult=null;
 		int contador = 0;
-		int maxnum=30;
+		int maxnum=15;
 
 		while(contador<maxnum){
 			try{
@@ -103,7 +103,6 @@ public class LyricSOAPorREST {
 				Response response= target.request(MediaType.APPLICATION_XML).get();
 				listResult= response.readEntity(ArrayOfSearchLyricResult.class).getSearchLyricResult();
 				if(listResult.get(0).getArtist()==null && listResult.get(0).getSong()==null) {
-					System.out.println("Não encontra!!!");
 					return null;
 				}
 				break;
@@ -111,15 +110,14 @@ public class LyricSOAPorREST {
 				//System.out.println("mensagem" + e.getMessage());
 				System.out.println(1);
 			}
+			contador++;
 		}
 
-	
 		try{
 			for(SearchLyricResult result:listResult ){
 				if(result.getSong().equalsIgnoreCase(song)){
 					idLyric=result.getLyricId();
 					lyricCheckSum=result.getLyricChecksum();
-					System.out.println(idLyric);
 					break;
 				}
 			}
@@ -142,9 +140,31 @@ public class LyricSOAPorREST {
 				//e.printStackTrace();
 				System.out.println(3);
 			}
+			contador++;
 		}
 
 		return null;
 	}
 
+	private static String lyricRESTWiki(String artist, String song)  {
+		String lyric="";
+		ResteasyClient cliente= new ResteasyClientBuilder().build();
+		int contador = 0;
+		int maxnum=15;
+
+		while(contador<maxnum){
+			try{
+				ResteasyWebTarget target=cliente.target("http://lyrics.wikia.com/api.php?artist="+artist+"&song="+song+"&fmt=xml");
+				Response response= target.request(MediaType.APPLICATION_XML).get();
+				lyric= response.readEntity(LyricsResult.class).getLyric();
+				return lyric;
+
+			}catch (Exception e){
+				//e.printStackTrace();
+				System.out.println(e.getMessage());
+			}
+			contador++;	
+		}
+		return null;
+	}
 }
